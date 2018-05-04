6009a55b055e1a0aa5771af443968a6487634706
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-04 13:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days){
-  return "It´s 20 days until Mother's Day!"
+  return "It's 20 days until Mother's Day!"
 }
