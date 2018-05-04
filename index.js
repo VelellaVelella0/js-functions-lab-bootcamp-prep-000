a7dc5fc7d0128066dd11d93252fd1d59b71a2139
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-04 13:01:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+
+
+function happyHolidaysTo(string) {
+ return "Happy holidays,${name}!"
+  }
+
+function happyHolidayTo(string) {
+  'Happy ${holidays},${name}!'
+}
+
+  function happyHolidaysTo(name) {
+    return string("Happy holidays, ${name}!")
+      happyHolidaysTo('name').toEqual('Happy holidays, you!')
+    }
